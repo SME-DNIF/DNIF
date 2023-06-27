cea7468d229ed7b013878fea660410814c4d4291
Manish5271 <91660847+Manish5271@users.noreply.github.com>
2023-06-27 11:27:36 +0530

    Update dfs_admin.sh

--- dfs_admin.sh
+++ dfs_admin.sh
@@ -24,6 +24,22 @@ else
 			echo -e "This script should be executed on the CORE server only!!!\n"
 		fi
 	fi
+
+  	if [[ $os == "centos" ]]; then
+		if [[ $(docker ps -a --format '{{.Names}}' | grep -w core-v9) == "core-v9" ]]; then
+			dateis=$(date +%d%m%Y)
+		        timeis=$(date +%H%M%S)
+		        uname=$(cat /DNIF/DL/csltuconfig/username)
+		        export HADOOP_USER_NAME=$uname
+		        cd /opt/hadoop-3.2.3/bin/
+        		./hdfs dfsadmin -report >> /var/tmp/dfsadmin_$dateis"T"$timeis.out
+			echo -e "\ndfsadmin report generated successfully!!!\n"
+			echo -e "path: /var/tmp/dfsadmin_$dateis"T"$timeis.out\n"
+		else
+			echo -e "\nThis is not the CORE server!!!\n"
+			echo -e "This script should be executed on the CORE server only!!!\n"
+		fi
+	fi
 	
 	if [[ $os == "rhel" ]]; then
                 if [[ $(podman ps -a --format '{{.Names}}' | grep -w core-v9) == "core-v9" ]]; then
